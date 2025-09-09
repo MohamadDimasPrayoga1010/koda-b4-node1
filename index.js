184c58b6f4f5fde98038788632b55c3164d3d11a
M.Dimas Prayoga <mdimasprayoga692@gmail.com>
2025-09-10 00:00:15 +0700

    update

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const namaMusic = require('./lib/generateMusic')
 const buatFile = require('./lib/pisahkanLagu')
 
 const music = [
-    'Wali - Neneku Pahlawanku',
-    'Wali - Cari Jodoh',
-    'Wali - Harga Diri',
-    'Wali - Yank',
-    'Slack - Virus',
-    'Slack - Ku Tak Bisa',
-    'Slack - Mawar Merah',
-    'Noah - Topeng',
-    'Noah - Semua Tentang kita',
-    'Setia Band - Cari Pacar Lagi'
+    'Wali-NenekuPahlawanku',
+    'Wali-CariJodoh',
+    'Wali-HargaDiri',
+    'Wali-Yank',
+    'Slack-Virus',
+    'Slack-KuTakBisa',
+    'Slack-MawarMerah',
+    'Noah-Topeng',
+    'Noah-SemuaTentangkita',
+    'SetiaBand-CariPacarLagi'
 ]
 
 // namaMusic(music);
